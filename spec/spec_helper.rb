7f273f31681641a319430c8230eed8f9f8cbe07d
Alex Schultz <aschultz@redhat.com>
2016-09-30 13:20:35 -0600

    Move rspec-puppet-facts to spec helper
    This change updates the module to use the rspec-puppet-facts as defined
    in the puppet-openstack_spec_helper.
    
    Change-Id: I9c7841f663d08057be641a814bf7afe4123bc0ac

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,6 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'shared_examples'
-
-require 'puppet-openstack_spec_helper/defaults'
-require 'rspec-puppet-facts'
-include RspecPuppetFacts
+require 'puppet-openstack_spec_helper/facts'
 
 RSpec.configure do |c|
   c.alias_it_should_behave_like_to :it_configures, 'configures'
