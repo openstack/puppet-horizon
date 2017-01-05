1a8c69d161da97f02e62e7d4c230c93521946a23
Harald Jensas <harald.jensas@gmail.com>
2017-01-05 03:10:59 +0100

    WebSSO config options
    Add support for WEBSSO options in local_settings.py.erb
    
    Cherry-pick from: review.openstack.org/#/c/416809
    Change-Id: Ie528eb1afeb967d30fc504cd682154913fe6927c
    Closes-Bug: #1607912

--- manifests/init.pp
+++ manifests/init.pp
@@ -279,6 +279,44 @@
 #    (optional) The timezone of the server.
 #    Defaults to 'UTC'.
 #
+#  [*websso_enabled*]
+#    (optional)Enable the WEBSSO_ENABLED option which turn on the keystone web
+#    single-sign-on if set to true.
+#    Default to false
+#
+#  [*websso_initial_choice*]
+#    (optional)Set the WEBSSO_INITIAL_CHOICE option used to determine which
+#    authentication choice to show as default.
+#    Defaults to undef
+#
+#  [*websso_choices*]
+#    (optional)Set the WEBSSO_CHOICES option, A list of authentication
+#    mechanisms which include keystone federation protocols and identity
+#    provide protocol mapping keys (WEBSSO_IDP_MAPPING).
+#    Default to undef
+#
+#    Example:
+#      websso_choices => [
+#        ['oidc', 'OpenID Connect'],
+#        ['saml2', 'Security Assertion Markup Language']
+#      ]
+#
+#  [*websso_idp_mapping*]
+#    (optional)Set the WEBSSO_IDP_MAPPING option.
+#    A dictionary of specific identity provider and protocol combinations.
+#    From theselected authentication mechanism, the value will be looked up as
+#    keys in the dictionary. If a match is found, it will redirect the user to
+#    a identity provider and federation protocol specific WebSSO endpoint in
+#    keystone, otherwise it will use the value as the protocol_id when
+#    redirecting to the WebSSO by protocol endpoint.
+#    Default to undef
+#
+#    Example:
+#      websso_idp_mapping => {
+#        'acme_oidc'  => ['acme', 'oidc'],
+#        'acme_saml2' => ['acme', 'saml2'],
+#      }
+#
 # === Examples
 #
 #  class { 'horizon':
@@ -343,6 +381,10 @@ class horizon(
   $root_url                            = $::horizon::params::root_url,
   $session_timeout                     = 1800,
   $timezone                            = 'UTC',
+  $websso_enabled                      = false,
+  $websso_initial_choice               = undef,
+  $websso_choices                      = undef,
+  $websso_idp_mapping                  = undef,
   # DEPRECATED PARAMETERS
   $can_set_mount_point                 = undef,
   $vhost_extra_params                  = undef,
