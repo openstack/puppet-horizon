fc6226565b9f4d4865e6ced0e001514521315fd9
Marc Methot <mb.methot@gmail.com>
2020-01-22 16:25:53 -0500

    Adding the HTTPONLY cookie header to secure_cookies
    Change-Id: Ic34170530b260b426fd65ac96aa5f494591c2ff1
    Closes-Bug: #1860608

--- spec/classes/horizon_init_spec.rb
+++ spec/classes/horizon_init_spec.rb
@@ -152,6 +152,7 @@ describe 'horizon' do
           "SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')",
           'CSRF_COOKIE_SECURE = True',
           'SESSION_COOKIE_SECURE = True',
+          'SESSION_COOKIE_HTTPONLY = True',
           "  'identity': 2.0,",
           "OPENSTACK_KEYSTONE_MULTIDOMAIN_SUPPORT = True",
           "OPENSTACK_KEYSTONE_DEFAULT_DOMAIN = 'domain.tld'",
