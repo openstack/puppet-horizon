382877dd4c8ee321eff1846c7c7afe4104dbd58f
Takashi Kajinami <tkajinam@redhat.com>
2021-02-12 16:28:49 +0900

    Add support for independent heat-dashboard configurations
    Currently horizon supports per-dashboard configurations in
    /etc/openstack-dashboard/local_settings.d. Each dashboard plugin
    provides a specific file to define its parameters, and any parameters
    used in dashboard plugins have been removed from the base
    local_settings(.py) in horizon repo.
    
    To follow how each plugin is supposed to have its configurations
    managed, this change introduces an independent file for heat-dashboard.
    Support for the other dashboards like manila-ui, octavia-ui, and etc
    will be added later.
    
    Change-Id: I5999c2eaf8d7788d3c0d76e69a56a0b10058dfe9

---
+++ spec/classes/horizon_dashboards_heat_spec.rb
@@ -0,0 +1,90 @@
+require 'spec_helper'
+
+describe 'horizon::dashboards::heat' do
+
+  let :params do
+    {}
+  end
+
+  shared_examples_for 'horizon::dashboards::heat' do
+
+    context 'with default parameters' do
+      let(:pre_condition) do
+        <<-eos
+          class { 'horizon':
+            secret_key => 'elj1IWiLoWHgcyYxFVLj7cM5rGOOxWl0',
+          }
+eos
+      end
+
+      it 'installs heat-dashboard package' do
+        is_expected.to contain_package('heat-dashboard').with(
+          :ensure => 'present',
+          :name   => platform_params[:heat_dashboard_package_name],
+          :tag    => ['openstack', 'horizon-package']
+        )
+      end
+
+      it 'generates _1699_orchestration_settings.py' do
+        verify_concat_fragment_contents(catalogue, '_1699_orchestration_settings.py', [
+          "settings.POLICY_FILES.update({",
+          "    'orchestration': 'heat_policy.yaml',",
+          "})"
+        ])
+        verify_concat_fragment_contents(catalogue, '_1699_orchestration_settings.py', [
+          "OPENSTACK_HEAT_STACK = {",
+          "    'enable_user_pass': True,",
+          "}",
+        ])
+      end
+    end
+
+    context 'with enable_user_pass disabled' do
+      let(:pre_condition) do
+        <<-eos
+          class { 'horizon':
+            secret_key => 'elj1IWiLoWHgcyYxFVLj7cM5rGOOxWl0',
+          }
+eos
+      end
+
+      before do
+        params.merge!({ :enable_user_pass => false })
+      end
+
+      it {
+        verify_concat_fragment_contents(catalogue, '_1699_orchestration_settings.py', [
+          "OPENSTACK_HEAT_STACK = {",
+          "    'enable_user_pass': False,",
+          "}",
+        ])
+      }
+    end
+
+    context 'without the horizon class defined' do
+      it { should raise_error(Puppet::Error) }
+    end
+  end
+
+  on_supported_os({
+    :supported_os => OSDefaults.get_supported_os
+  }).each do |os,facts|
+    context "on #{os}" do
+      let (:facts) do
+        facts.merge!(OSDefaults.get_facts())
+      end
+
+      let(:platform_params) do
+        case facts[:osfamily]
+        when 'Debian'
+          { :heat_dashboard_package_name => 'python3-heat-dashboard' }
+        when 'RedHat'
+          { :heat_dashboard_package_name => 'openstack-heat-ui' }
+        end
+      end
+
+      it_behaves_like 'horizon::dashboards::heat'
+    end
+  end
+
+end
