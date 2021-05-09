c99e9fdb46531d2e0d8ffc57ca72fe65703932fb
Takashi Kajinami <tkajinam@redhat.com>
2021-05-09 11:14:01 +0900

    Add support for independent manila-dashboard configurations
    This change introduces the new horizon::dashboards::manila class,
    which allows users to manage the independent configuration file for
    manila dashboard (_90_manila_shares.py).
    
    Conflicts:
            manifests/params.pp
    
    Change-Id: Ie1442d34b6837ced5f44018d038d4d716d568655
    (cherry picked from commit a7d71fb37d7638d19f9ba22650a49c93b2be9142)
    (cherry picked from commit fd8db39d8dd66b4895a181a1d187a7c555fbfe83)

---
+++ manifests/dashboards/manila.pp
@@ -0,0 +1,93 @@
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# == Class: horizon::dashboards::manila
+#
+# Manage parameters of manila-dashboard
+#
+# === Parameters:
+#
+#  [*policy_file*]
+#    (optional) Local copy of service policy files.
+#    Defaults to 'manila_policy.yaml'
+#
+#  [*manila_options*]
+#    (optional) A hash of parameters to enable features specific to Manila.
+#    These include;
+#    'enable_share_groups': Boolean
+#    'enable_replication': Boolean
+#    'enable_migration': Boolean
+#    'enable_public_share_type_creation': Boolean
+#    'enable_public_share_group_type_creation': Boolean
+#    'enable_public_shares': Boolean
+#    'enabled_share_protocols': Array
+#
+class horizon::dashboards::manila(
+  $policy_file    = 'manila_policy.yaml',
+  $manila_options = {}
+) {
+
+  include horizon::deps
+  include horizon::params
+
+  # The horizon class should be included so that some common parameters
+  # can be picked here.
+  if ! defined(Class[horizon]) {
+    fail('The horizon class should be included before the horizon::dashboards::manila class')
+  }
+  $log_handlers = $::horizon::log_handlers
+  $log_level    = $::horizon::log_level
+  $policy_files = $::horizon::policy_files
+
+  if $policy_files and $policy_files['share'] {
+    $policy_file_real = $policy_files['share']
+  } else {
+    $policy_file_real = $policy_file
+  }
+
+  # Default options for the OPENSTACK_MANILA_FEATURES section. These will
+  # be merged with user-provided options when the _90_manila_shares.py.erb
+  # template is interpolated.
+  $manila_defaults = {
+    'enable_share_groups'                     => true,
+    'enable_replication'                      => true,
+    'enable_migration'                        => true,
+    'enable_public_share_type_creation'       => true,
+    'enable_public_share_group_type_creation' => true,
+    'enable_public_shares'                    => true,
+    'enabled_share_protocols'                 => ['NFS', 'CIFS', 'GlusterFS', 'HDFS', 'CephFS', 'MapRFS'],
+  }
+  $manila_options_real = merge($manila_defaults, $manila_options)
+
+  $config_file = "${::horizon::params::conf_d_dir}/_90_manila_shares.py"
+
+  package { 'manila-dashboard':
+    ensure => $::horizon::package_ensure,
+    name   => $::horizon::params::manila_dashboard_package_name,
+    tag    => ['openstack', 'horizon-package'],
+  }
+
+  concat { $config_file:
+    mode    => '0640',
+    owner   => $::horizon::params::wsgi_user,
+    group   => $::horizon::params::wsgi_group,
+    require => File[$::horizon::params::conf_d_dir],
+    tag     => ['django-config'],
+  }
+
+  concat::fragment { '_90_manila_shares.py':
+    target  => $config_file,
+    content => template('horizon/_90_manila_shares.py.erb'),
+    order   => '50',
+  }
+}
